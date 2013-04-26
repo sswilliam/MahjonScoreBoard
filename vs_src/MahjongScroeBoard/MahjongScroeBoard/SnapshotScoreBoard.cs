487e71caba54ad264a481b18dfb1c09edab86ca7
yi <sswilliam@live.cn>
2013-04-26 10:51:21 +0800

    add setup project

--- vs_src/MahjongScroeBoard/MahjongScroeBoard/SnapshotScoreBoard.cs
+++ vs_src/MahjongScroeBoard/MahjongScroeBoard/SnapshotScoreBoard.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
-using System.Drawing;
 using System.Collections;
 
 namespace MahjongScroeBoard
