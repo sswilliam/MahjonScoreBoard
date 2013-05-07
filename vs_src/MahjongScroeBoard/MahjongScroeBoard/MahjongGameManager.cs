27e42387a5b6ecd53b0b94364ff5d2abe3814b89
sswilliam <sswilliam@live.cn>
2013-05-08 00:02:40 +0800

    revise the code

--- vs_src/MahjongScroeBoard/MahjongScroeBoard/MahjongGameManager.cs
+++ vs_src/MahjongScroeBoard/MahjongScroeBoard/MahjongGameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace MahjongScroeBoard
 {
@@ -11,6 +12,7 @@ namespace MahjongScroeBoard
         private IntPtr QQMJPtr = IntPtr.Zero;
         private MahjongGameManager()
         {
+            Console.WriteLine(Environment.OSVersion.ToString());
         }
         public static MahjongGameManager getInstance()
         {
@@ -29,9 +31,48 @@ namespace MahjongScroeBoard
         {
             return null;
         }
-        private Bitmap doTakeSnapshot()
+
+        private static int takeTime = 0;
+        private Bitmap doPrintWindowTakeSnapshot()
         {
-            return null;
+            if (takeTime > 3)
+            {
+                return null;
+            }
+            takeTime++;
+            try
+            {
+                if (QQMJPtr == IntPtr.Zero)
+                {
+                    syncMJHandler();
+                    if (QQMJPtr == IntPtr.Zero)
+                    {
+                        return null;
+                    }
+                }
+
+                Rectangle rect = new Rectangle();
+                Native.GetWindowRect(QQMJPtr, out rect);
+                Bitmap bt = GetWindow(QQMJPtr, rect.Width - rect.X, rect.Height - rect.Y);
+                bt.Save("test.jpg", ImageFormat.Jpeg);
+                return bt;
+            }
+            catch (Exception e)
+            {
+                return doPrintWindowTakeSnapshot();
+            }
+        }
+        private static Bitmap GetWindow(IntPtr hWnd, int width, int height)
+        {
+            IntPtr hscrdc = Native.GetWindowDC(hWnd);
+            IntPtr hbitmap = Native.CreateCompatibleBitmap(hscrdc, width, height);
+            IntPtr hmemdc = Native.CreateCompatibleDC(hscrdc);
+            Native.SelectObject(hmemdc, hbitmap);
+            Native.PrintWindow(hWnd, hmemdc, 0);
+            Bitmap bmp = Bitmap.FromHbitmap(hbitmap);
+            Native.DeleteDC(hscrdc);
+            Native.DeleteDC(hmemdc);
+            return bmp;
         }
     }
 }
